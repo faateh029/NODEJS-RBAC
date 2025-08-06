0ffd8430d5c1cfe4cac3caf78be970c31122572a
faateh029 <buttfatih691@gmail.com>
2025-08-06 15:39:04 +0500

    Tested login and register API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,18 @@
-import {User} from '../models/userModel.js';
-export const register = async (req,res)=>{
+import User from '../models/userModel.js';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+export const registerController = async (req,res)=>{
     try {
         const {username , password , role} = req.body;
+        console.log(username,password,role);
   const checksUserName = await User.findOne({username:username});
+  console.log(checksUserName);
   if(checksUserName){
     return res.status(409).json({msg:"Conflict: Username already exists"})
   }
-  const hashPass = await bcrypt.hash(password , 10);
-  await User.save({username, password:hashPass , role});
+  const hashPass = await bcryptjs.hash(password , 10);
+  console.log(hashPass);
+  await User.create({username, password:hashPass , role});
     res.status(201).json({msg:"User Created Successfully"})
     } catch (error) {
         return res.status(500).json({msg:"Internal Server Error"});
@@ -15,20 +20,24 @@ export const register = async (req,res)=>{
   
 }
 
-export const login = async (req,res)=>{
+export const loginController = async (req,res)=>{
     try {
      const {username , password } = req.body;
+     console.log(username , password);
      
   const user = await User.findOne({username:username});
+  console.log(user);
   if(!user){
     return res.status(409).json({msg:"Credentials are incorrect"})
   }
   const isMatch = await bcryptjs.compare(password , user.password);
+   console.log(isMatch);
   if(!isMatch){
     return res.status(409).json({msg:"Credentials are incorrect"})
   }
-  const token = await Jwt.sign({id:user._id , role:user.role} , process.env.JWT_SECRET, {expiresIn:"1h"})
-  res.status(201).json({msg:"User logged in successfully"});
+  const token = await jwt.sign({id:user._id , role:user.role} , process.env.JWT_SECRET, {expiresIn:"1h"})
+  console.log(token);
+  res.status(201).json({token:token});
      
     } catch (error) {
         return res.status(500).json({msg:"Something went wrong"});
--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { authRouter } from './routes/authRoutes.js';
 import { dbConnect } from './config/dbConnect.js';
 dotenv.config();
-const port = process.env.PORT||7001 
+const port = process.env.PORT||7001 ;
 dbConnect();
 const app = express();
 
 //middleware
 
 app.use(express.json());
-
+app.use('/' , authRouter);
 //routes
 
 
