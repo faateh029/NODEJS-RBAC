aac8b0cf082f28ebcefd77ca172ec5b948efbfae
faateh029 <buttfatih691@gmail.com>
2025-08-06 14:09:35 +0500

    Implement register API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,18 @@
+import {User} from '../models/userModel.js';
 export const register = async (req,res)=>{
-
+    try {
+        const {username , password , role} = req.body;
+  const checksUserName = await User.findOne({username:username});
+  if(checksUserName){
+    return res.status(409).json({msg:"Conflict: Username already exists"})
+  }
+  const hashPass = await bcrypt.hash(password , 10);
+  await User.save({username, password:hashPass , role});
+    res.status(201).json({msg:"User Created Successfully"})
+    } catch (error) {
+        return res.status(500).json({msg:"Internal Server Error"});
+    }
+  
 }
 
 export const login = async (req,res)=>{
