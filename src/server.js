49e902eb40aa347534b0de9d97a758c446ac803d
faateh029 <buttfatih691@gmail.com>
2025-08-06 12:51:33 +0500

    Setup server and install dependencies

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT||7001;
+const app = express();
+
+//middleware
+
+app.use(express.json());
+
+//routes
+
+
+//port
+
+app.listen(port , ()=>{
+    console.log(`server running on port ${port}`);
+})
