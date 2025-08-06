c9cacb20dc44fcfc5a7b64cf313d0b25a3abf045
faateh029 <buttfatih691@gmail.com>
2025-08-06 17:13:09 +0500

    Protecting user routes

---
+++ src/middlewares/authMid.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+export const verifyToken = async (req,res , next)=>{
+    let authHeader = req.headers.authorization ||req.headers.Authorization;
+    let accessToken;
+    if(authHeader && authHeader.startsWith("Bearer"))
+    {
+        accessToken = authHeader.split(" ")[1];
+        if(!accessToken){
+            return res.status(400).json({msg:"Token not found"});
+        }
+        try {
+        const decode = jwt.verify(accessToken ,process.env.JWT_SECRET);
+        req.user = decode;  
+        console.log(`the decoded user is ${decode}`);
+        next();
+
+        } catch (error) {
+            return res.status(500).json({msg:"server error"})
+        }
+    }
+
+
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import {Router} from 'express';
 import{adminController,userController,managerController } from '../controllers/userControllers.js';
+import {verifyToken} from '../middlewares/authMid.js';
 export const userRouter = new Router();
 
-userRouter.post('/admin' , adminController);
-userRouter.post('/user' , userController);
-userRouter.post('/manager' , managerController);
+userRouter.post('/admin',verifyToken , adminController);
+userRouter.post('/user' , verifyToken,userController);
+userRouter.post('/manager' ,verifyToken, managerController);
