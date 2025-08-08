6439d45ee01e4576536edadd4af62d4a4e92521b
faateh029 <buttfatih691@gmail.com>
2025-08-08 19:15:03 +0500

    logger implemented and tested

--- src/config/logger.js
+++ src/config/logger.js
@@ -4,14 +4,12 @@ const logger = winston.createLogger({
     format:winston.format.combine(
         winston.format.prettyPrint(),
         winston.format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),
-        winston.format.printf((level,message,timestamp)=>{
-            return `[${timestamp}] ${level.toUpperCase()}: , ${message}`;
-        })
-    ),
+       winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+),
     transports:[
-        new winston.transports.Console(),
-        new winston.transports.File({filename:'/logs/error.log',level:'error'}),
-        new winston.transports.File({filename:'/logs/combined.log'})
+        new winston.transports.Console()
     ]
 });
 export default logger;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,26 @@
 import User from '../models/userModel.js';
+import logger from '../config/logger.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 export const registerController = async (req,res)=>{
     try {
         const {username , password , role} = req.body;
-        console.log(username,password,role);
+       // console.log(username,password,role);
   const checksUserName = await User.findOne({username:username});
-  console.log(checksUserName);
+  //console.log(checksUserName);
   if(checksUserName){
+     logger.warn('Cannot access the username because it already exists')
     return res.status(409).json({msg:"Conflict: Username already exists"})
   }
+  logger.info('username checked');
   const hashPass = await bcryptjs.hash(password , 10);
-  console.log(hashPass);
+  logger.info('password hashed');
+//  console.log(hashPass);
   await User.create({username, password:hashPass , role});
+    logger.info('new user registered ');
     res.status(201).json({msg:"User Created Successfully"})
     } catch (error) {
+      logger.error("server error");
         return res.status(500).json({msg:"Internal Server Error"});
     }
 
@@ -26,16 +32,21 @@ export const loginController = async (req,res)=>{
      console.log(username , password);
      
   const user = await User.findOne({username:username});
-  console.log(user);
+  //console.log(user);
   if(!user){
     return res.status(409).json({msg:"Credentials are incorrect"})
   }
+
   const isMatch = await bcryptjs.compare(password , user.password);
    console.log(isMatch);
   if(!isMatch){
+    logger.warn('credentials are incorrect')
     return res.status(409).json({msg:"Credentials are incorrect"})
   }
+      logger.info('user verified')
+
   const token = await jwt.sign({id:user._id , role:user.role} , process.env.JWT_SECRET, {expiresIn:"1h"})
+  logger.info('token generted')
   // const token = await jwt.sign({id:user._id} , process.env.JWT_SECRET, {expiresIn:"1h"})
   // console.log(token);
   res.status(201).json({token:token});
